19dc0095c62f4d0bd91a68e24fed99afda39b8d3
na1ara <naiarabarceloss@gmail.com>
2022-10-29 11:39:39 -0300

    parametros, set senha e traduz senha

--- intruso.hpp
+++ intruso.hpp
@@ -1,9 +1,18 @@
+#ifndef INTRUSO_H
+#define INTRUSO_H
+
 #include <string>
+#include <vector>
 
 class Intruso{
     /*Continue a implementação da classe Intruso*/
+    std::vector<std::string> senhas_vazadas;
+    std::vector<std::string> traduzidas;
 
     public:
         void set_senha_vazada(std::string vazou);
+        std::vector<std::string> traduz_senha(int tam);
         std::string crack_senha();
 };
+
+#endif
